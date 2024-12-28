fde390ad30a4a9817c3c30a60a7a5c7ebb704ee2
venkataniharbillakurthi <venkataniharbillakurthi@gmail.com>
2024-12-28 17:45:58 +0530

    Add GitHub Pages deployment configuration

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx';
 import './index.css';
 import { StateContextProvider } from './Context/index.jsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 
 
 const queryClient = new QueryClient();
@@ -13,7 +13,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <QueryClientProvider client={queryClient}>
     <StateContextProvider>
         <App />
-      <ReactQueryDevtools/>
     </StateContextProvider>
   </QueryClientProvider>
 );
